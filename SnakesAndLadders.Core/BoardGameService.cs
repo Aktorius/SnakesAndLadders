ad8359913fd32fe77f03cacaa94d669ff64dc4a2
miri.tarik@gmail.com <tarik.miri@bookingtek.com>
2019-02-26 18:15:39 +0000

    Add contract and class for board game service

---
+++ SnakesAndLadders.Core/BoardGameService.cs
@@ -0,0 +1,18 @@
+﻿using SnakesAndLadders.Core.Contracts;
+using SnakesAndLadders.Models;
+
+namespace SnakesAndLadders.Core
+{
+    public class BoardGameService : IBoardGameService
+    {
+        public void PlayTurn(Token token, int spaces)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsGameWon()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SnakesAndLadders.Core/Contracts/IBoardGameService.cs
@@ -0,0 +1,10 @@
+﻿using SnakesAndLadders.Models;
+
+namespace SnakesAndLadders.Core.Contracts
+{
+    public interface IBoardGameService
+    {
+        void PlayTurn(Token token, int spaces);
+        bool IsGameWon();
+    }
+}
