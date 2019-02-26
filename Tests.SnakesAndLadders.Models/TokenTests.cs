53de7e8764fcf65e6caa4b66c349ae57bb5df0ab
miri.tarik@gmail.com <tarik.miri@bookingtek.com>
2019-02-26 17:46:48 +0000

    Create test class for Token model and add test

---
+++ Tests.SnakesAndLadders.Models/TokenTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.SnakesAndLadders.Models
+{
+    [TestClass]
+    public class TokenTests
+    {
+        [TestMethod]
+        public void Given_A_Token_Is_Created_The_Position_Should_Be_1()
+        {
+            // Given
+            int expectedSquarePosition = 1;
+
+            // When
+            var token = new Token();
+
+            // Then
+            Assert.AreEqual(token.SquarePosition, expectedSquarePosition);
+        }
+    }
+}
