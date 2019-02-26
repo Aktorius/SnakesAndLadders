dd90ef956e64a6aa053e664a7d0097d7707b13c0
miri.tarik@gmail.com <tarik.miri@bookingtek.com>
2019-02-26 18:03:36 +0000

    Create contract and concrete class for dice service

---
+++ SnakesAndLadders.Core/DiceService.cs
@@ -0,0 +1,12 @@
+﻿using SnakesAndLadders.Core.Contracts;
+
+namespace SnakesAndLadders.Core
+{
+    public class DiceService : IDiceService
+    {
+        public int RollDie(int maxValue)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
