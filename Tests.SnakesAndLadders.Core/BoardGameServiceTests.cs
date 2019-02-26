fd4d9fb9c56ceb72d8eb49807712d29296abf1d2
miri.tarik@gmail.com <tarik.miri@bookingtek.com>
2019-02-26 18:13:36 +0000

    Add test to validate when the game is won

--- Tests.SnakesAndLadders.Core/BoardGameServiceTests.cs
+++ Tests.SnakesAndLadders.Core/BoardGameServiceTests.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnakesAndLadders.Models;
 
 namespace Tests.SnakesAndLadders.Core
 {
     [TestClass]
     public class BoardGameServiceTests
     {
+        [TestMethod]
+        public void Given_A_Token_Is_Square_97_And_Move_3_Then_The_Game_Is_Won()
+        {
+            // Given
+            var token = new Token
+            {
+                SquarePosition = 97
+            };
+
+            var boardGameService = new BoardGameService();
+
+            // When
+            boardGameService.PlayTurn(token, 3);
+
+            // Then
+            Assert.IsTrue(boardGameService.IsGameWon());
+        }
     }
 }
