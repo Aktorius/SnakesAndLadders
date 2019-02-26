8e23196ba95b61555a0879274ea7c16b60bc7748
miri.tarik@gmail.com <tarik.miri@bookingtek.com>
2019-02-26 18:01:00 +0000

    Add test for rolling a die

--- Tests.SnakesAndLadders.Core/DiceServiceTests.cs
+++ Tests.SnakesAndLadders.Core/DiceServiceTests.cs
@@ -5,6 +5,22 @@ namespace Tests.SnakesAndLadders.Core
     [TestClass]
     public class DiceServiceTests
     {
+        [TestMethod]
+        public void Given_A_Die_Is_Rolled_Then_Result_Should_Be_Between_1_And_6()
+        {
+            // Given
+            var diceFaces = 6;
+            var diceService = new DiceService();
 
+            var expectedLowerBound = 1;
+            var expectedHigherBound = 6;
+
+            // When
+            var result = diceService.RollDie();
+
+            // Then
+            Assert.IsTrue(expectedLowerBound <= result);
+            Assert.IsTrue(expectedHigherBound >= result);
+        }
     }
 }
